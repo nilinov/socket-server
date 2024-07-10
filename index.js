4f13e9537b27f02d357d1df4e62f30e11ac78bfe
Nicolay I <nicolay-developer@yandex.ru>
2024-07-10 10:03:00 +0700

    Подставил event_id вместо user_id

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const io = new Server({});
 
 const clients = {};
 
-async function checkUser(headers = { token: '', user_id: '', socket }) {
+async function checkUser(headers = { token: '', event_id: '', socket }) {
     try {
-        // TODO отпраивть запрос для проверки корректности токена и соответствия user_id тому, что указан в токене
-        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${headers.user_id}?token=${headers.token}`).then(e => e.json())
-        clients[headers.user_id] = headers.socket;
+        // TODO отпраивть запрос для проверки корректности токена и соответствия event_id тому, что указан в токене
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${headers.event_id}?token=${headers.token}`).then(e => e.json())
+        clients[headers.event_id] = headers.socket;
     } catch (e) {
         console.error(`Не удалось проверить корерктность токена`, e);
     }
@@ -21,7 +21,7 @@ io.on("connection", (socket) => {
   checkUser({
     token: socket.client.request.headers['token'],
     socket: socket,
-    user_id: socket.client.request.headers['user_id'],
+    event_id: socket.client.request.headers['event_id'],
   })
 
   socket.on('event1', (event) => {
@@ -46,8 +46,8 @@ app.use(express.json());
 
 app.post('/', function(request, response){
 
-    if (request.body.userId) {
-        clients[request.body.userId].emit('re-send', request.body)
+    if (request.body.event_id) {
+        clients[request.body.event_id].emit('re-send', request.body)
     }
 
     console.log(request.body);      // your JSON
